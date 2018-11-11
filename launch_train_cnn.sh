65362c8241d2c813319a32983c297e6861b5b70a
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2018-11-11 22:59:34 +0000

    update batch

--- launch_train_cnn.sh
+++ launch_train_cnn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#sbatch --job-name=KQH_cnn --gres=gpu:0 --mem=65536 --cpus-per-task=4 --output=output/output_train_cnn.out launch_train_cnn.sh
+#sbatch --job-name=KQ_cnn --gres=gpu:1 --mem=32000 --cpus-per-task=4 --output=output/output_train_cnn_3_gpu_with_logs.out launch_train_cnn.sh
 
 python3 train_cnn.py
