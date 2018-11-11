eaed551afe6ffd8dce25ba5f66cbfcf6f0fd91a8
Kaiqiang <kaiqianghuang666@gmail.com>
2018-11-11 19:07:35 +0000

    add ouput

--- launch_train_all_models.sh
+++ launch_train_all_models.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#sbatch --job-name=KQH_lstm --gres=gpu:0 --mem=65536 --cpus-per-task=4 --output=output/output_train_all_models.out launch_train_all_models.sh
+#sbatch --job-name=KQH_lstm --gres=gpu:0 --mem=32000 --cpus-per-task=4 --output=output/output_train_lstm_lrcn.out launch_train_all_models.sh
 
 
 python3 train.py -m lstm
 
-python3 train.py -m lcrn
+python3 train.py -m lrcn
 
-python3 train.py -m mlp
+#python3 train.py -m mlp
 
 #python3 train.py -m c3d
 
