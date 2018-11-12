90e50cfa1f5c56a022009d43ca17f385eb5e7a26
Kaiqiang <kaiqianghuang666@gmail.com>
2018-11-12 09:29:34 +0000

    modify

--- validate_cnn_testset.py
+++ validate_cnn_testset.py
@@ -17,14 +17,14 @@ def main(nb_images=5):
     # CNN model evaluate
 
     test_data_gen = ImageDataGenerator(rescale=1. / 255)
-    test_data_num = 186640 #the number of test images
+    test_data_num = 697865 #the number of test images
     batch_size = 32
     test_generator = test_data_gen.flow_from_directory('/data/d14122793/ucf101_full/test/', target_size=(299, 299),
                                                        batch_size=batch_size, classes=data.classes,
                                                        class_mode='categorical')
     # load the trained model that has been saved in CNN_train_UCF101.py, your model name maybe is not the same as follow
     model = load_model('/data/d14122793/ucf101_full/checkpoints/inception.022-1.61.hdf5')
-    results = model.evaluate_generator(generator=test_generator, steps=100)
+    results = model.evaluate_generator(generator=test_generator, steps=test_data_num // batch_size)
     print(results)
     print(model.metrics)
 
