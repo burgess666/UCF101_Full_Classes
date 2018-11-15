4db73b55ddba64fdae0c6a10d24645cf2777b316
Kaiqiang Huang <kaiqianghuang666@gmail.com>
2018-11-15 13:29:13 +0000

    update

--- launch_train_all_models.sh
+++ launch_train_all_models.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#sbatch --job-name=KQ_lstm --gres=gpu:1 --mem=4096 --cpus-per-task=2 --output=output/output_train_lstm.out launch_train_all_models.sh
+#sbatch --job-name=KQ_lrcn --gres=gpu:1 --mem=4096 --cpus-per-task=2 --output=output/output_train_lrcn.out launch_train_all_models.sh
 
 
-python3 train.py -m lstm
+#python3 train.py -m lstm
 
-#python3 train.py -m lrcn
+python3 train.py -m lrcn
 
 #python3 train.py -m mlp
 
